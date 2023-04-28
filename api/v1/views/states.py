51cc5bc353f58985b8ae227c2a724271be1c7b99
purity <shishchei@gmail.com>
2023-04-28 14:14:20 -0700

    CRUD method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -16,7 +16,7 @@ def states(state_id=None):
     statesObj = storage.all(State)
 
     new_state = [obj.to_dict() for obj in statesObj.values()]
-    if state_id is None:
+    if not state_id:
         if request.method == 'GET':
             return jsonify(new_state)
         elif request.method == 'POST':
@@ -26,4 +26,32 @@ def states(state_id=None):
                 abort(400, 'Not a JSON')
             if new_dict.get("name") is None:
                 abort(400, 'Missing name')
+            created_state = State(**new_dict)
+            created_state.save()
+            return jsonify(created_state.to_dict()), 201
+    else:
+        if request.method == 'GET':
+            for state in new_state:
+                if state.get('id') == state_id:
+                    return jsonify(state)
+            abort(404)
 
+        elif request.method == 'PUT':
+            new_dict = request.get_json()
+
+            if new_dict is None:
+                abort(400, 'Not a JSON')
+            for state in statesObj.values():
+                if state.id == state_id:
+                    state.name = new_dict.get("name")
+                    state.save()
+                    return jsonify(state.to_dict()), 200
+            abort(404)
+
+        elif request.method == 'DELETE':
+            for obj in statesObj.values():
+                if obj.id == state_id:
+                    storage.delete(obj)
+                    storage.save()
+                    return jsonify({}), 200
+            abort(404)
