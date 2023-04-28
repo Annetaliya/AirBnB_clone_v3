ac61399c6458f82e54247cfdc1803d1106bf3014
purity <shishchei@gmail.com>
2023-04-28 12:42:47 -0700

    added files

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 '''falsk framework'''
 
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 
 
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def clean_up(exception=None):
     '''eliminate session'''
     storage.close()
+
+
+@app.errorhandler(404)
+def error_handler(error):
+    '''handling 404 error'''
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=True)
---
+++ api/v1/views/states.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+'''objects that handles all default RESTFul API actions'''
+
